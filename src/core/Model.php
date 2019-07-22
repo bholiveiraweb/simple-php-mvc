1e42dda1cf989fa5cc5bcda482b0096df944bd16
Bruno Henrique F. Oliveira <bholiveira.web@gmail.com>
2019-07-21 23:19:14 -0300

    Fixing base model

--- src/core/Model.php
+++ src/core/Model.php
@@ -5,7 +5,7 @@ use CoffeeCode\DataLayer\DataLayer;
 
 class Model extends DataLayer
 {
-    public function __construct(string $entity, array $required, string $primary = 'id', bool $timestamps = true)
+    public function __construct(string $entity, array $required, string $primary = null, bool $timestamps = null)
     {
         parent::__construct($entity, $required, $primary, $timestamps);
     }
